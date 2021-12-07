01ace187d281dd2e17ffacae1d08bd9495098b13
mirkousuelli <mirko.usuelli@mail.polimi.it>
2021-12-07 19:17:21 +0100

    calibration done

--- 2 - Geometry/G2 - Calibration/test_calibration.m
+++ 2 - Geometry/G2 - Calibration/test_calibration.m
@@ -9,48 +9,42 @@ close all;
 img = imread('../../img/villa_image.png');
 img_gray = rgb2gray(img);
 
-%norm_factor = max((size(img)));
-%norm_matrix = diag([1/norm_factor, 1/norm_factor, 1]);
+%IMG_MAX_SIZE = max(size(im_rgb));
 
 % interactively select f families of segments that are images of 3D parallel lines
 lines = findLines(img_gray);
 
 % CONSTRUCTION LINES
-idx = [3, 19];
-label = ['n', 'r']
-%fig = figure();
-%imshow(img), hold on;
-%for k = 1:length(idx)
-%    xy = [lines(idx(k)).point1; lines(idx(k)).point2];
-%    plot(xy(:,1),xy(:,2),'LineWidth',1,'Color', 'red');
-%    text(xy(:,1),xy(:,2), label(k), 'FontSize', 20, 'Color', 'red'); 
-%end
-%hold off;
-line_n = cross([lines(idx(1)).point1 1]', [lines(idx(1)).point2 1]');
+idx = [23, 17, 20, 29, 7, 2];
+label = ['m', 'n', 'p', 'q', 'r', 's'];
+fig = figure();
+imshow(img), hold on;
+for k = 1:length(idx)
+    xy = [lines(idx(k)).point1; lines(idx(k)).point2];
+    plot(xy(:,1),xy(:,2),'LineWidth',1,'Color', 'red');
+    text(xy(:,1),xy(:,2), label(k), 'FontSize', 20, 'Color', 'red'); 
+end
+hold off;
+
+line_m = cross([lines(idx(1)).point1 1]', [lines(idx(1)).point2 1]');
+line_m = line_m / line_m(3);
+line_n = cross([lines(idx(2)).point1 1]', [lines(idx(2)).point2 1]');
 line_n = line_n / line_n(3);
-line_r = cross([lines(idx(2)).point1 1]', [lines(idx(2)).point2 1]');
-line_r = line_r / line_r(3);
-V0 = cross(line_n, line_r);
+V0 = cross(line_m, line_n);
 V0 = V0 / V0(3);
-V0 = V0';% * norm_matrix;
 
-A = [226 334 1];%; * norm_matrix;
-B = [272 534 1];%; * norm_matrix;
-E = [731 551 1];%; * norm_matrix;
-G = [821 364 1];%; * norm_matrix;
-
-line_t1 = cross(A, B);
-line_t1 = line_t1 / line_t1(3);
-line_t2 = cross(G, E);
-line_t2 = line_t2 / line_t2(3);
-V1 = cross(line_t1, line_t2);
+line_p = cross([lines(idx(3)).point1 1]', [lines(idx(3)).point2 1]');
+line_p = line_p / line_p(3);
+line_q = cross([lines(idx(4)).point1 1]', [lines(idx(4)).point2 1]');
+line_q = line_q / line_q(3);
+V1 = cross(line_p, line_q);
 V1 = V1 / V1(3);
 
-line_s1 = cross(A, G);
-line_s1 = line_s1 / line_s1(3);
-line_s2 = cross(B, E);
-line_s2 = line_s2 / line_s2(3);
-V2 = cross(line_s1, line_s2);
+line_r = cross([lines(idx(5)).point1 1]', [lines(idx(5)).point2 1]');
+line_r = line_r / line_r(3);
+line_s = cross([lines(idx(6)).point1 1]', [lines(idx(6)).point2 1]');
+line_s = line_s / line_s(3);
+V2 = cross(line_r, line_s);
 V2 = V2 / V2(3);
 
 V_line = cross(V1, V2);
@@ -65,17 +59,17 @@ H_met = [1.2256,-0.3136, 0;
          0,      0,      1.0000;];
      
 H = H_met * H_aff;
-H_inv = inv(H);
+H = inv(H);
 
 % Using L_inf, vertical vp and homography
-IAC = get_IAC(V_line', V0', [], [], H_inv);
+IAC = get_IAC(V_line, V0, V1, V2, H);
 
 % get the intrinsic parameter before the denormalization
 alfa = sqrt(IAC(1,1));
 u0 = -IAC(1,3)/(alfa^2);
 v0 = -IAC(2,3);
-fy = sqrt(abs(IAC(3,3) - (alfa^2)*(u0^2) - (v0^2)));
-fx = fy /alfa;
+fy = sqrt(IAC(3,3) - (alfa^2)*(u0^2) - (v0^2));
+fx = fy / alfa;
 
 % build K using the parametrization
 K = [fx 0 u0; 0 fy v0; 0 0 1];
